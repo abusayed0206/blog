1f915d54941649339da9c20bf581d227d486fbec
MD. ABU SAYED <hi@abusayed.dev>
2023-02-20 22:33:57 +0600

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'SAYED's Blog',
+  siteName: 'SAYED Blog',
   siteDescription: 'Personal Blog for SAYED to write about his thoughts and ideas',
 
   templates: {
